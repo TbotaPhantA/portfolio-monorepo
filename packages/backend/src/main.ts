3b7e760949c8707f12c640342a5f7065ea65cbd8
TbotaPhantA <botakiller@mail.ru>
2025-07-06 19:46:06 +0000

    swagger log was added

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, new DocumentBuilder().build());
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, () => console.log(`Swagger: http://localhost:${port}/api`));
 }
 bootstrap();
