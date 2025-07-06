803f226ee730e15ee4f72666dd680a0c36a89936
TbotaPhantA <botakiller@mail.ru>
2025-07-06 17:50:26 +0000

    .env was added for main.ts

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config.js';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, () => {
+    if (process.env.ENV === 'dev') {
+      console.log(`Swagger: http://localhost:${port}/api`);
+    }
+  });
 }
 bootstrap();
