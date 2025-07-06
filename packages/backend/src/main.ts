e52677cc43b19b18b6cce56be6145de3021c07d4
TbotaPhantA <botakiller@mail.ru>
2025-07-06 19:33:06 +0000

    swagger was added to main

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -5,12 +5,17 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const documentFactory = () => SwaggerModule.createDocument(app, new DocumentBuilder().build());
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
