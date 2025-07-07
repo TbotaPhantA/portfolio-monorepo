1b90d05aa94bf33bfbd0befedaeff12d6523b661
TbotaPhantA <botakiller@mail.ru>
2025-07-07 12:13:22 +0000

    TypeORM's decorators were added

--- packages/backend/src/auth/domain/user/user.ts
+++ packages/backend/src/auth/domain/user/user.ts
@@ -1,14 +1,44 @@
 import { UserRoleEnum } from '../enums/userRole.enum';
 import { NoMethods } from '../../../infrastructure/shared/types/noMethods';
 import { RefreshToken } from './refreshToken/refreshToken';
+import { Column, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'users' })
 export class User {
+  @PrimaryGeneratedColumn({ type: 'smallint', name: 'user_id' })
   userId: number;
+
+  @Column({
+    type: 'enum',
+    enum: UserRoleEnum,
+    name: 'role',
+    nullable: false,
+  })
   role: UserRoleEnum;
+
+  @Column({
+    type: 'smallint',
+    name: 'access_tokens_version',
+    default: () => '1',
+  })
   accessTokensVersion: number;
+
+  @Column({
+    type: 'varchar',
+    length: 31,
+    unique: true,
+    nullable: false,
+  })
   username: string;
+
+  @Column({ type: 'varchar', nullable: false })
   salt: string;
+
+  @Column({ type: 'varchar', name: 'password_hash', nullable: false })
   passwordHash: string;
+
+  @OneToMany(() => RefreshToken, rt => rt.userId)
+  @JoinColumn({ name: 'user_id' })
   refreshTokens: RefreshToken[];
 
   constructor(raw: NoMethods<User>) {
--- packages/backend/src/posts/domain/post/post.ts
+++ packages/backend/src/posts/domain/post/post.ts
@@ -3,17 +3,43 @@ import { PostTypeEnum } from '../enums/postType.enum';
 import { LanguageEnum } from '../enums/language.enum';
 import type { NoMethods } from '../../../infrastructure/shared/types/noMethods';
 import { Comment } from './comment/comment';
+import { Column, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'posts' })
 export class Post {
+  @PrimaryGeneratedColumn({ type: 'smallint', name: 'post_id' })
   postId: number;
+
+  @Column({ type: 'smallint', name: 'user_id' })
   userId: number;
+
+  @Column({ type: 'enum', enum: PostStatusEnum, nullable: false })
   status: PostStatusEnum;
+
+  @Column({ type: 'enum', enum: PostTypeEnum, nullable: false })
   type: PostTypeEnum;
+
+  @Column({ type: 'enum', enum: LanguageEnum, nullable: false })
   language: LanguageEnum;
+
+  @Column({
+    type: 'timestamptz',
+    name: 'created_at',
+    default: () => 'now()',
+  })
   createdAt: Date;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
   title: string;
+
+  @Column({ type: 'text', nullable: false })
   body: string;
+
+  @Column('text', { array: true, nullable: true })
   tags: string[];
+
+  @OneToMany(() => Comment, rt => rt.postId)
+  @JoinColumn({ name: 'post_id' })
   comments: Comment[];
 
   constructor(raw: NoMethods<Post>) {
