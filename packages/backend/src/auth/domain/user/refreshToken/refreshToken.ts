f06794c05a21051b7bac340bd718fb4e2b71a642
TbotaPhantA <botakiller@mail.ru>
2025-07-08 17:06:18 +0000

    TypeORM was removed

--- packages/backend/src/auth/domain/user/refreshToken/refreshToken.ts
+++ packages/backend/src/auth/domain/user/refreshToken/refreshToken.ts
@@ -1,18 +1,9 @@
 import { NoMethods } from '../../../../infrastructure/shared/types/noMethods';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'refresh_tokens' })
 export class RefreshToken {
-  @PrimaryGeneratedColumn({ type: 'smallint', name: 'refresh_token_id' })
   refreshTokenId: number;
-
-  @Column({ type: 'smallint', name: 'user_id' })
   userId: number;
-
-  @Column({ type: 'timestamp', name: 'expires_at' })
   expiresAt: Date;
-
-  @Column({ type: 'varchar', nullable: false })
   token: string;
 
   constructor(raw: NoMethods<RefreshToken>) {
--- packages/backend/src/posts/domain/post/comment/comment.ts
+++ packages/backend/src/posts/domain/post/comment/comment.ts
@@ -1,28 +1,11 @@
 import { NoMethods } from '../../../../infrastructure/shared/types/noMethods';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'comments' })
 export class Comment {
-  @PrimaryGeneratedColumn({ type: 'smallint', name: 'comment_id' })
   commentId: number;
-
-  @Column({ type: 'smallint', name: 'post_id' })
   postId: number;
-
-  @Column({ type: 'smallint', name: 'user_id' })
   userId: number;
-
-  @Column({
-    type: 'timestamptz',
-    name: 'created_at',
-    default: () => 'now()',
-  })
   createdAt: Date;
-
-  @Column({ type: 'ltree', nullable: false })
   path: string;
-
-  @Column({ type: 'text', nullable: false })
   body: string;
 
   constructor(raw: NoMethods<Comment>) {
