7f75552750eb706f5d3e8eec7147c315c4844749
TbotaPhantA <botakiller@mail.ru>
2025-07-11 18:58:00 +0000

    minor test refactoring

--- packages/backend/test/unit/auth/domain/user/user.spec.ts
+++ packages/backend/test/unit/auth/domain/user/user.spec.ts
@@ -43,84 +43,80 @@ const makeExpectedPayload = (
 
 describe(`${User.name}`, () => {
   describe(`${User.prototype.login.name}`, () => {
-    describe('happy path', () => {
-      const notThrowsTestCases = [
-        {
-          name: '1 valid admin credentials - should return valid tokens',
-          user: UserBuilder.defaultAll.with({
-            passwordHash: makePasswordHash(
-              'correct password',
-              AuthConfigBuilder.defaultAll.result,
-            ),
-          }).result,
-          suppliedPassword: 'correct password',
-          authConfig: AuthConfigBuilder.defaultAll.result,
-          now: new Date(2022, 0, 3),
-        },
-      ];
+    const notThrowsTestCases = [
+      {
+        name: '1 valid admin credentials - should return valid tokens',
+        user: UserBuilder.defaultAll.with({
+          passwordHash: makePasswordHash(
+            'correct password',
+            AuthConfigBuilder.defaultAll.result,
+          ),
+        }).result,
+        suppliedPassword: 'correct password',
+        authConfig: AuthConfigBuilder.defaultAll.result,
+        now: new Date(2022, 0, 3),
+      },
+    ];
 
-      test.each(notThrowsTestCases)(
-        '$name',
-        async ({ user, suppliedPassword, authConfig, now }) => {
-          // arrange
-          jest.useFakeTimers().setSystemTime(now);
+    test.each(notThrowsTestCases)(
+      '$name',
+      async ({ user, suppliedPassword, authConfig, now }) => {
+        // arrange
+        jest.useFakeTimers().setSystemTime(now);
 
-          // act
-          const resultTokens = await user.login(suppliedPassword, authConfig);
+        // act
+        const resultTokens = await user.login(suppliedPassword, authConfig);
 
-          // assert
-          const tokensPayload = assertTokensAreValid();
-          expect(tokensPayload).toStrictEqual(makeExpectedPayload(user, authConfig, now));
-          expect(user.refreshTokens[user.refreshTokens.length - 1])
-            .toStrictEqual(
-              RefreshTokenBuilder.defaultPreInserted.with({
-                token: resultTokens.refreshToken,
-                expiresAt: new Date(now.getTime() + authConfig.refreshToken.expiryInSeconds * 1000)
-              }).result
-            )
+        // assert
+        const tokensPayload = assertTokensAreValid();
+        expect(tokensPayload).toStrictEqual(makeExpectedPayload(user, authConfig, now));
+        expect(user.refreshTokens[user.refreshTokens.length - 1])
+          .toStrictEqual(
+            RefreshTokenBuilder.defaultPreInserted.with({
+              token: resultTokens.refreshToken,
+              expiresAt: new Date(now.getTime() + authConfig.refreshToken.expiryInSeconds * 1000)
+            }).result
+          )
 
-          function assertTokensAreValid() {
-            const accessPayload = jwt.verify(
-              resultTokens.accessToken,
-              authConfig.accessToken.privateKey,
-            );
-            const refreshPayload = jwt.verify(
-              resultTokens.refreshToken,
-              authConfig.refreshToken.privateKey,
-            );
+        function assertTokensAreValid() {
+          const accessPayload = jwt.verify(
+            resultTokens.accessToken,
+            authConfig.accessToken.privateKey,
+          );
+          const refreshPayload = jwt.verify(
+            resultTokens.refreshToken,
+            authConfig.refreshToken.privateKey,
+          );
 
-            return { accessPayload, refreshPayload };
-          }
-        },
-      );
-    });
+          return { accessPayload, refreshPayload };
+        }
+      },
+    );
 
-    describe('throws test cases', () => {
-      const throwsTestCases = [
-        {
-          name: '1 invalid password - should throw',
-          user: UserBuilder.defaultAll.with({
-            passwordHash: makePasswordHash(
-              'correct password',
-              AuthConfigBuilder.defaultAll.result,
-            ),
-          }).result,
-          suppliedPassword: 'incorrect password',
-          authConfig: AuthConfigBuilder.defaultAll.result,
-          now: new Date(2022, 0, 3),
-        },
-      ];
+    const throwsTestCases = [
+      {
+        name: '1 invalid password - should throw',
+        user: UserBuilder.defaultAll.with({
+          passwordHash: makePasswordHash(
+            'correct password',
+            AuthConfigBuilder.defaultAll.result,
+          ),
+        }).result,
+        suppliedPassword: 'incorrect password',
+        authConfig: AuthConfigBuilder.defaultAll.result,
+        now: new Date(2022, 0, 3),
+      },
+    ];
 
-      test.each(throwsTestCases)(
-        '$name',
-        async ({ user, suppliedPassword, authConfig, now }) => {
-          jest.useFakeTimers().setSystemTime(now);
+    test.each(throwsTestCases)(
+      '$name',
+      async ({ user, suppliedPassword, authConfig, now }) => {
+        jest.useFakeTimers().setSystemTime(now);
 
-          await expect(
-            user.login(suppliedPassword, authConfig),
-          ).rejects.toThrow(USERNAME_OR_PASSWORD_IS_NOT_VALID);
-        },
-      );
-    });
+        await expect(
+          user.login(suppliedPassword, authConfig),
+        ).rejects.toThrow(USERNAME_OR_PASSWORD_IS_NOT_VALID);
+      },
+    );
   });
 })
