e1358278a6787f6c4d309edd3ac820e4961ef465
TbotaPhantA <botakiller@mail.ru>
2025-07-12 16:34:35 +0000

    login response dto was marked for swagger

--- packages/backend/src/auth/domain/dto/loginResponse.dto.ts
+++ packages/backend/src/auth/domain/dto/loginResponse.dto.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginResponseDto {
+  @ApiProperty()
   accessToken: string;
+
+  static from(accessToken: string): LoginResponseDto {
+    const dto = new LoginResponseDto()
+    dto.accessToken = accessToken;
+    return dto;
+  }
 }
