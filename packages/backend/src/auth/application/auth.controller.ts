145c0a5d99603f6a2158f9cc4cabbf3ad33ec61c
TbotaPhantA <botakiller@mail.ru>
2025-07-12 16:33:15 +0000

    login method was added to auth controller

--- packages/backend/src/auth/application/auth.controller.ts
+++ packages/backend/src/auth/application/auth.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { LoginDto } from '../domain/dto/login.dto';
+import { LoginResponseDto } from '../domain/dto/loginResponse.dto';
+import { AuthService } from './auth.service';
+import { ApiResponse } from '@nestjs/swagger';
+import { FastifyReply } from 'fastify';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly auth: AuthService) {}
+
   @Post('/login')
-  async login() {}
+  @ApiResponse({ type: LoginResponseDto })
+  async login(
+    @Body() dto: LoginDto,
+    @Res({ passthrough: true }) reply: FastifyReply,
+  ): Promise<LoginResponseDto> {
+    return this.auth.login(dto, reply);
+  }
   /**
    * 1. /auth/login
    * 2. /auth/refresh
