7dbb453b32603d48d91fb9aca0f33a5a3f0058bb
TbotaPhantA <botakiller@mail.ru>
2025-07-12 22:13:48 +0000

    findByUsername fixes

--- packages/backend/src/auth/application/user/user.repository.ts
+++ packages/backend/src/auth/application/user/user.repository.ts
@@ -1,40 +1,55 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { Database } from '../../../infrastructure/db/db.schema';
-import { Kysely } from 'kysely';
+import { Kysely, sql, } from 'kysely';
 import { InjectKysely } from 'nestjs-kysely';
 import { User } from '../../domain/user/user';
 import { RefreshToken } from '../../domain/user/refreshToken/refreshToken';
+import { jsonBuildObject } from 'kysely/helpers/postgres'
 
 @Injectable()
 export class UserRepository {
   constructor(@InjectKysely() private readonly db: Kysely<Database>) {}
 
   async findByUsername(username: string): Promise<User | null> {
-    const raw = await this.db
+    const query = this.db
       .selectFrom('users')
       .leftJoin('refresh_tokens', 'users.user_id', 'refresh_tokens.user_id')
-      .select([
-        'users.user_id as userId',
-        'users.jwt_tokens_version as jwtTokensVersion',
-        'users.roles as roles',
-        'users.username as username',
-        'users.salt as salt',
-        'users.password_hash as passwordHash',
-        this.db.fn.jsonAgg('refresh_tokens').as('refreshTokens'),
-      ])
+      .select(({ fn, ref }) => {
+        return [
+          'users.user_id as userId',
+          'users.jwt_tokens_version as jwtTokensVersion',
+          'users.roles as roles',
+          'users.username as username',
+          'users.salt as salt',
+          'users.password_hash as passwordHash',
+          fn.coalesce(
+            fn.jsonAgg(
+              jsonBuildObject({
+                refresh_token_id: ref('refresh_tokens.refresh_token_id'),
+                user_id: ref('refresh_tokens.user_id'),
+                expires_at: ref('refresh_tokens.expires_at'),
+                token: ref('refresh_tokens.token'),
+              })
+            ).filterWhere('refresh_tokens.refresh_token_id', 'is not', null),
+            sql`'[]'`
+          ).as('refresh_tokens')
+        ]
+      })
       .where('users.username', '=', username)
       .groupBy(['users.user_id'])
-      .executeTakeFirst();
+    
+    const raw = await query.executeTakeFirst();
 
     if (!raw) return null;
 
-    const refreshTokens = raw.refreshTokens.map(
+    const refreshTokens = raw.refresh_tokens.map(
       (t) =>
         new RefreshToken({
-          refreshTokenId: t.refresh_token_id,
-          userId: t.user_id,
-          expiresAt: t.expires_at,
-          token: t.token,
+          refreshTokenId: t.refresh_token_id!,
+          userId: t.user_id!,
+          expiresAt: t.expires_at!,
+          token: t.token!,
         }),
     );
 
