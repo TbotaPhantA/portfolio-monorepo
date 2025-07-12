5a87ba7883e7cc8369ee046b93633a8c59df3261
TbotaPhantA <botakiller@mail.ru>
2025-07-12 14:13:27 +0000

    db module was added to app module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/application/auth.module';
 import { PostsModule } from './posts/application/posts.module';
+import { DBModule } from './infrastructure/db/db.module';
 
 @Module({
-  imports: [AuthModule, PostsModule],
+  imports: [DBModule, AuthModule, PostsModule],
   controllers: [],
   providers: [],
 })
