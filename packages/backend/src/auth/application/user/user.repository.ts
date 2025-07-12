d1c545061beacef7c53ca1cdad8bb9f4879c483c
TbotaPhantA <botakiller@mail.ru>
2025-07-12 16:36:09 +0000

    findByUsername method was implemented

--- packages/backend/src/auth/application/user/user.repository.ts
+++ packages/backend/src/auth/application/user/user.repository.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { Database } from '../../../infrastructure/db/db.schema';
+import { Kysely } from 'kysely';
+import { InjectKysely } from 'nestjs-kysely';
+import { User } from '../../domain/user/user';
+import { RefreshToken } from '../../domain/user/refreshToken/refreshToken';
 
 @Injectable()
-export class UserRepository {}
+export class UserRepository {
+  constructor(@InjectKysely() private readonly db: Kysely<Database>) {}
+
+  async findByUsername(username: string): Promise<User | null> {
+    const raw = await this.db
+      .selectFrom('users')
+      .leftJoin('refresh_tokens', 'users.user_id', 'refresh_tokens.user_id')
+      .select([
+        'users.user_id as userId',
+        'users.jwt_tokens_version as jwtTokensVersion',
+        'users.roles as roles',
+        'users.username as username',
+        'users.salt as salt',
+        'users.password_hash as passwordHash',
+        this.db.fn
+          .jsonAgg('refresh_tokens')
+          .as('refreshTokens'),
+      ])
+      .where('users.username', '=', username)
+      .groupBy(['users.user_id'])
+      .executeTakeFirst()
+
+    if (!raw) return null;
+
+    const refreshTokens = raw.refreshTokens.map(
+      (t) =>
+        new RefreshToken({
+          refreshTokenId: t.refresh_token_id,
+          userId: t.user_id,
+          expiresAt: t.expires_at,
+          token: t.token,
+        })
+    )
+
+    return new User({
+      userId: raw.userId,
+      jwtTokensVersion: raw.jwtTokensVersion,
+      roles: raw.roles as any,
+      username: raw.username,
+      salt: raw.salt,
+      passwordHash: raw.passwordHash,
+      refreshTokens,
+    })
+  }
+}
