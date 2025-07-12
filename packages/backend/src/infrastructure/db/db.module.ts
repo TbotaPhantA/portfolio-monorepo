ee326d259a4fde9b44d5c4ec1a08d9ca1e81a93c
TbotaPhantA <botakiller@mail.ru>
2025-07-12 16:32:59 +0000

    db module was initialized

---
+++ packages/backend/src/infrastructure/db/db.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { PostgresDialect } from 'kysely';
+import { KyselyModule } from 'nestjs-kysely';
+import { Pool } from 'pg';
+import { config } from '../config/config';
+
+const kyselyModule = KyselyModule.forRoot({
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      database: config.db.name,
+      user: config.db.username,
+      password: config.db.password,
+      host: config.db.host,
+      port: config.db.port,
+      max: 10,
+    })
+  })
+})
+
+@Module({
+  imports: [kyselyModule],
+  exports: [kyselyModule]
+})
+export class DBModule {}
