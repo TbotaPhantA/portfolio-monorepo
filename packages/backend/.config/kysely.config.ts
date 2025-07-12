b6da9ff18dba484c78bb30e8b2dddcd1ddc14404
TbotaPhantA <botakiller@mail.ru>
2025-07-12 15:16:39 +0000

    config was migratinos was added

---
+++ packages/backend/.config/kysely.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'kysely-ctl'
+import { PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
+
+export default defineConfig({
+	// replace me with a real dialect instance OR a dialect name + `dialectConfig` prop.
+	dialect: new PostgresDialect({
+    pool: new Pool({
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      max: 10,
+    })
+  }),
+	  migrations: {
+	    migrationFolder: "migrations",
+	  },
+	  seeds: {
+	    seedFolder: "seeds",
+	  }
+	//   plugins: [],
+})
