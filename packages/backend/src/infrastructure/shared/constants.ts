e57e265876740d1190a66911c664f3b61f5a2e30
TbotaPhantA <botakiller@mail.ru>
2025-07-13 13:31:46 +0000

    REFRESH_TOKEN_NOT_FOUND error message was added

--- packages/backend/src/infrastructure/shared/constants.ts
+++ packages/backend/src/infrastructure/shared/constants.ts
@@ -4,3 +4,4 @@ export const PLACEHOLDER_DATE = new Date(1);
 // error messages
 export const USERNAME_OR_PASSWORD_IS_NOT_VALID =
   'USERNAME_OR_PASSWORD_IS_NOT_VALID';
+export const REFRESH_TOKEN_NOT_FOUND = 'REFRESH_TOKEN_NOT_FOUND';
