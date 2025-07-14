2a7debd27214dfffa435fccbe94cd673e4f5ced8
TbotaPhantA <botakiller@mail.ru>
2025-07-14 19:12:08 +0000

    insertAndFillInPost method was implement in the repository

--- packages/backend/src/posts/application/posts.repository.ts
+++ packages/backend/src/posts/application/posts.repository.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Database } from '../../infrastructure/db/db.schema';
+import { Kysely } from 'kysely';
+import { Post } from '../domain/post/post';
 
 @Injectable()
-export class PostsRepository {}
+export class PostsRepository {
+  constructor(private readonly db: Kysely<Database>) {}
+
+  async insertAndFillInPost(post: Post): Promise<void> {
+    const result = await this.db
+      .insertInto('posts')
+      .values({
+        user_id: post.userId,
+        status: post.status,
+        type: post.type,
+        language: post.language,
+        title: post.title,
+        body: post.body,
+        tags: post.tags,
+      })
+      .returning(['post_id', 'created_at'])
+      .executeTakeFirstOrThrow()
+
+    post.postId = result.post_id;
+    post.createdAt = result.created_at;
+  }
+}
