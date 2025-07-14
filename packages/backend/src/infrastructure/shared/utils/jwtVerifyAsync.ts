80d6c9c243f9b851097acea577b79bda17c6ff58
TbotaPhantA <botakiller@mail.ru>
2025-07-14 19:46:45 +0000

    jwtVerifyAsync function was added

---
+++ packages/backend/src/infrastructure/shared/utils/jwtVerifyAsync.ts
@@ -0,0 +1,9 @@
+import * as jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import { GetPublicKeyOrSecret, PublicKey, Secret, VerifyOptions } from 'jsonwebtoken';
+
+export const jwtVerifyAsync = promisify(jwt.verify) as (
+  token: string,
+  secretOrPublicKey: Secret | PublicKey | GetPublicKeyOrSecret,
+  options?: VerifyOptions & { complete?: false },
+) => ReturnType<typeof jwt.verify>;
