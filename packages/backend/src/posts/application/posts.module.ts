b8c1b8ce0f9c652544f9aa495f9ed0b76c4df9ba
TbotaPhantA <botakiller@mail.ru>
2025-07-14 19:48:34 +0000

    posts repository to was added to providers list

--- packages/backend/src/posts/application/posts.module.ts
+++ packages/backend/src/posts/application/posts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsCreateService } from './services/postsCreate.service';
+import { PostsRepository } from './posts.repository';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsCreateService],
+  providers: [PostsCreateService, PostsRepository],
 })
 export class PostsModule {}
