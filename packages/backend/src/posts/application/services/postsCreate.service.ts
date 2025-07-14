cc84ac8edcfa44c59c3281d2edab933d0e2c6daa
TbotaPhantA <botakiller@mail.ru>
2025-07-14 19:12:24 +0000

    create method in PostsCreate was implemented

--- packages/backend/src/posts/application/services/postsCreate.service.ts
+++ packages/backend/src/posts/application/services/postsCreate.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PostResponseDto } from '../../domain/dto/createPost/postResponse.dto';
+import { CreatePostDto } from '../../domain/dto/createPost/createPost.dto';
+import { Post } from '../../domain/post/post';
+import { UserPayload } from '../../../infrastructure/shared/types/userPayload';
+import { PostsRepository } from '../posts.repository';
 
 @Injectable()
-export class PostsCreateService {}
+export class PostsCreateService {
+  constructor(private readonly repo: PostsRepository) {}
+
+  async create(dto: CreatePostDto, user: UserPayload): Promise<PostResponseDto> {
+    const post = Post.createByDto(dto, user);
+    await this.repo.insertAndFillInPost(post);
+    return PostResponseDto.from(post);
+  }
+}
