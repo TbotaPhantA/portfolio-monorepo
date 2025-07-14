fa573faa37db4804d2aa0ea1dcb55e676b5f3a7a
TbotaPhantA <botakiller@mail.ru>
2025-07-14 23:52:52 +0000

    api property for posts was added

--- packages/backend/src/posts/domain/dto/search/searchPostsResponseDto.ts
+++ packages/backend/src/posts/domain/dto/search/searchPostsResponseDto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PostResponseDto } from '../createPost/postResponse.dto';
 
 export class SearchPostsResponseDto {
+  @ApiProperty({ type: PostResponseDto })
   posts: PostResponseDto[];
 
   static from(
