f1d94de34254737c3e4284ae7b9ade2a112514c6
TbotaPhantA <botakiller@mail.ru>
2025-07-14 19:10:11 +0000

    create post dto was modified

--- packages/backend/src/posts/domain/dto/createPost/createPost.dto.ts
+++ packages/backend/src/posts/domain/dto/createPost/createPost.dto.ts
@@ -1,14 +1,38 @@
 import { PostStatusEnum } from '../../enums/postStatus.enum';
 import { PostTypeEnum } from '../../enums/postType.enum';
 import { LanguageEnum } from '../../enums/language.enum';
-import { CreateCommentDto } from './createComment.dto';
+import { IsArray, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @IsNotEmpty()
+  @IsEnum(PostStatusEnum)
+  @ApiProperty()
   status: PostStatusEnum;
+
+  @IsNotEmpty()
+  @IsEnum(PostTypeEnum)
+  @ApiProperty()
   type: PostTypeEnum;
+
+  @IsNotEmpty()
+  @IsEnum(LanguageEnum)
+  @ApiProperty()
   language: LanguageEnum;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
   body: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  @IsString({ each: true })
+  @ApiProperty()
   tags: string[];
-  comments: CreateCommentDto[];
 }
