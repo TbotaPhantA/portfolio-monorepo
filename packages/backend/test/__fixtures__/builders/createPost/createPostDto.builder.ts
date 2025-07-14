78011071e108c209c19a880f20b5410a9289819d
TbotaPhantA <botakiller@mail.ru>
2025-07-14 19:11:24 +0000

    comment is not created during post creation

--- packages/backend/test/__fixtures__/builders/createPost/createPostDto.builder.ts
+++ packages/backend/test/__fixtures__/builders/createPost/createPostDto.builder.ts
@@ -3,7 +3,6 @@ import { CreatePostDto } from '../../../../src/posts/domain/dto/createPost/creat
 import { PostStatusEnum } from '../../../../src/posts/domain/enums/postStatus.enum';
 import { PostTypeEnum } from '../../../../src/posts/domain/enums/postType.enum';
 import { LanguageEnum } from '../../../../src/posts/domain/enums/language.enum';
-import { CommentBuilder } from '../post/comment.builder';
 
 export class CreatePostDtoBuilder {
   static get defaultAll(): InjectionBuilder<CreatePostDto> {
@@ -14,7 +13,6 @@ export class CreatePostDtoBuilder {
       title: 'title',
       body: 'body',
       tags: ['tag1'],
-      comments: [CommentBuilder.defaultPreInserted.result],
     });
   }
 }
