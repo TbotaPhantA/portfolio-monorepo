268d963242baf16608daede326d364e074726d1d
TbotaPhantA <botakiller@mail.ru>
2025-07-14 19:48:25 +0000

    createPost method was added

--- packages/backend/src/posts/application/posts.controller.ts
+++ packages/backend/src/posts/application/posts.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { CreatePostDto } from '../domain/dto/createPost/createPost.dto';
+import { PostResponseDto } from '../domain/dto/createPost/postResponse.dto';
+import { PostsCreateService } from './services/postsCreate.service';
+import { Authentication } from '../../auth/application/decorators/authentication';
+import { UserPayload } from '../../infrastructure/shared/types/userPayload';
+import { UserRoleEnum } from '../../auth/domain/enums/userRole.enum';
 
+@Authentication([UserRoleEnum.ADMIN])
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly createService: PostsCreateService) {}
+
+  @Post()
+  async createPost(
+    @Body() dto: CreatePostDto,
+    @Req() { user }: { user: UserPayload }, // TODO: validate user payload
+  ): Promise<PostResponseDto> {
+    return this.createService.create(dto, user);
+  }
+}
