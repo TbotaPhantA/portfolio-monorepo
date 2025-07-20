76af886d568b1d57bb6f6c18656a8b487651f15f
TbotaPhantA <botakiller@mail.ru>
2025-07-20 20:36:34 +0000

    uneeded comment was removed

--- packages/backend/src/auth/application/guards/auth.guard.ts
+++ packages/backend/src/auth/application/guards/auth.guard.ts
@@ -12,7 +12,7 @@ import { config } from '../../../infrastructure/config/config';
 export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException();
