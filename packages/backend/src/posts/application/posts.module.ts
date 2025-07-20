52d6dd899b95de2aec3178206f6a66b74ce8497c
TbotaPhantA <botakiller@mail.ru>
2025-07-20 20:39:19 +0000

    UpdatePostsService was added

--- packages/backend/src/posts/application/posts.module.ts
+++ packages/backend/src/posts/application/posts.module.ts
@@ -3,9 +3,15 @@ import { PostsController } from './posts.controller';
 import { CreatePostsService } from './services/createPosts.service';
 import { PostsRepository } from './posts.repository';
 import { ReadPostsService } from './services/readPosts.service';
+import { UpdatePostsService } from './services/updatePosts.service';
 
 @Module({
   controllers: [PostsController],
-  providers: [ReadPostsService, CreatePostsService, PostsRepository],
+  providers: [
+    ReadPostsService,
+    CreatePostsService,
+    UpdatePostsService,
+    PostsRepository,
+  ],
 })
 export class PostsModule {}
