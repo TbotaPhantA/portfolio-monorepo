e5c18d1cee9c29e41bf82071d281bce06b7078cc
TbotaPhantA <botakiller@mail.ru>
2025-07-20 20:39:36 +0000

    findOneById and update method were added

--- packages/backend/src/posts/application/posts.repository.ts
+++ packages/backend/src/posts/application/posts.repository.ts
@@ -1,15 +1,80 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { Database } from '../../infrastructure/db/db.schema';
-import { Kysely } from 'kysely';
+import { Kysely, sql } from 'kysely';
 import { Post } from '../domain/post/post';
 import { InjectKysely } from 'nestjs-kysely';
 import { SearchPostsParams } from '../domain/dto/search/searchPostsParamDto';
 import { SearchPostsResponseDto } from '../domain/dto/search/searchPostsResponseDto';
+import { jsonBuildObject } from 'kysely/helpers/postgres';
+import { Comment } from '../domain/post/comment/comment';
 
 @Injectable()
 export class PostsRepository {
   constructor(@InjectKysely() private readonly db: Kysely<Database>) {}
+
+  async findOneById(postId: number): Promise<Post | null> {
+    const post = await this.db
+      .selectFrom('posts')
+      .leftJoin('comments', 'posts.postId', 'comments.postId')
+      .select(({ fn, ref }) => {
+        return [
+        'posts.postId',
+        'posts.userId',
+        'posts.status',
+        'posts.type',
+        'posts.language',
+        'posts.createdAt',
+        'posts.title',
+        'posts.body',
+        'posts.tags',
+          fn
+            .coalesce(
+              fn
+                .jsonAgg(
+                  jsonBuildObject({
+                    commentId: ref('comments.commentId'),
+                    postId: ref('comments.postId'),
+                    userId: ref('comments.userId'),
+                    createdAt: ref('comments.createdAt'),
+                    path: ref('comments.path'),
+                    body: ref('comments.body'),
+                  }),
+                )
+                .filterWhere('comments.commentId', 'is not', null),
+              sql<never[]>`'[]'`,
+            )
+            .as('comments'),
+        ]
+      })
+      .where('posts.postId', '=', postId)
+      .groupBy('posts.postId')
+      .executeTakeFirst()
+
+      if (!post) return null;
+
+      const comments = post.comments.map(p => new Comment({
+        commentId: p.commentId!,
+        postId: p.postId!,
+        userId: p.userId!,
+        createdAt: p.createdAt!,
+        path: p.path!,
+        body: p.body!,
+      }));
+
+      return new Post({
+        postId: post.postId,
+        userId: post.userId,
+        status: post.status,
+        type: post.type,
+        language: post.language,
+        createdAt: post.createdAt,
+        title: post.title,
+        body: post.body,
+        tags: post.tags,
+        comments,
+      });
+  }
 
   async searchPosts(search: SearchPostsParams): Promise<SearchPostsResponseDto> {
     let query = this.db
@@ -60,5 +125,15 @@ export class PostsRepository {
 
     post.postId = result.postId;
     post.createdAt = result.createdAt;
+  }
+
+  async update(post: Post): Promise<void> {
+    const changes = post.changes();   
+
+    await this.db
+      .updateTable('posts')
+      .where('posts.postId', '=', post.postId)
+      .set(changes)
+      .execute()
   }
 }
